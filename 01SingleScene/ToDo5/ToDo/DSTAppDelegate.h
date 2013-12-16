2490a788a5a795ff57ef8ffc2083b1c16ae2d816
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-16 12:33:34 -0500

    Split project after moving todo3 to todo4

---
+++ 01SingleScene/ToDo5/ToDo/DSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSTAppDelegate.h
+//  ToDo
+//
+//  Created by Daniel Steinberg on 12/14/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
