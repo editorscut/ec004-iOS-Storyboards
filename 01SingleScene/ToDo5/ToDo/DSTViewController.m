7a0cf8eafe13755dd6fefaea66d7a50c08ac804e
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-16 16:08:32 -0500

    Set action in todo6

--- 01SingleScene/ToDo5/ToDo/DSTViewController.m
+++ 01SingleScene/ToDo5/ToDo/DSTViewController.m
@@ -7,16 +7,12 @@
 
 @implementation DSTViewController
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
--- 01SingleScene/ToDo6/ToDo/DSTViewController.m
+++ 01SingleScene/ToDo6/ToDo/DSTViewController.m
@@ -7,6 +7,14 @@
 
 @implementation DSTViewController
 
+
+- (IBAction)addButtonTapped:(UIButton *)sender {
+    self.todoListView.text = [NSString stringWithFormat:@" %@\n%@", self.inputField.text, self.todoListView.text];
+    self.inputField.text = @"";
+    [self.inputField resignFirstResponder];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
