f8ceb2d5cd3339a71162bcf551e68179414d45fe
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-16 17:50:18 -0500

    Created delegate and implemented delegate method.

--- 01SingleScene/ToDo7/ToDo/DSTViewController.m
+++ 01SingleScene/ToDo7/ToDo/DSTViewController.m
@@ -1,17 +1,17 @@
 #import "DSTViewController.h"
 
-@interface DSTViewController ()
+@interface DSTViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *inputField;
 @property (weak, nonatomic) IBOutlet UITextView *todoListView;
 @end
 
 @implementation DSTViewController
 
-
-- (IBAction)addButtonTapped:(UIButton *)sender {
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
     self.todoListView.text = [NSString stringWithFormat:@" %@\n%@", self.inputField.text, self.todoListView.text];
     self.inputField.text = @"";
     [self.inputField resignFirstResponder];
+    return YES;
 }
 
 
