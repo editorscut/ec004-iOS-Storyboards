f8a2eb7cce90905d33b98c292bf2eb18341871fb
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-17 15:49:44 -0500

    Stage two of the Colors project

---
+++ 02Transitions/Colors2/Colors/DSTViewController.m
@@ -0,0 +1,35 @@
+//
+//  DSTViewController.m
+//  Colors
+//
+//  Created by Daniel Steinberg on 12/17/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import "DSTViewController.h"
+
+@interface DSTViewController ()
+
+@end
+
+@implementation DSTViewController
+- (IBAction)redButtonTapped:(UIButton *)sender {
+    UIViewController *redViewController = [[self storyboard] instantiateViewControllerWithIdentifier:@"redScene"];
+    [self presentViewController:redViewController
+                       animated:YES
+                     completion:nil];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
