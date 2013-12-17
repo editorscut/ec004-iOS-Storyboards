48d3092ed45ad6483cb51a8768a56361d7aff788
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-17 13:15:08 -0500

    First stage of project two.

---
+++ 02Transitions/Colors/Colors/DSTViewController.m
@@ -0,0 +1,29 @@
+//
+//  DSTViewController.m
+//  Colors
+//
+//  Created by Daniel Steinberg on 12/17/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import "DSTViewController.h"
+
+@interface DSTViewController ()
+
+@end
+
+@implementation DSTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
