abc1905d198caff3adaf0a69cfa4266cb2655de1
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-16 23:24:47 -0500

    Added Gesture recognizer.

---
+++ 01SingleScene/ToDo8/ToDo/DSTViewController.m
@@ -0,0 +1,33 @@
+#import "DSTViewController.h"
+
+@interface DSTViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UITextView *todoListView;
+@end
+
+@implementation DSTViewController
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    self.todoListView.text = [NSString stringWithFormat:@" %@\n%@", self.inputField.text, self.todoListView.text];
+    self.inputField.text = @"";
+    [self.inputField resignFirstResponder];
+    return YES;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)viewTapped:(UITapGestureRecognizer *)sender {
+    [self.inputField becomeFirstResponder];
+}
+
+@end
