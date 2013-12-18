120e16b743b51866b2c8f5425646bb2526e35496
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-18 14:00:11 -0500

    Added prepareForSegue:sender: to stage three.

---
+++ 03Forward/Forward3/Colors/DSTViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSTViewController.h
+//  Colors
+//
+//  Created by Daniel Steinberg on 12/17/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSTViewController : UIViewController
+
+@end
