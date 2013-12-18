98aa6c86382d73ec4cbb50d1d1eccd6c4ea6723e
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-18 13:51:44 -0500

    Re-colored red scene to red.

---
+++ 03Forward/Forward2/Colors/DSTViewController.m
@@ -0,0 +1,40 @@
+//
+//  DSTViewController.m
+//  Colors
+//
+//  Created by Daniel Steinberg on 12/17/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import "DSTViewController.h"
+
+@interface DSTViewController ()
+
+@end
+
+@implementation DSTViewController
+- (IBAction)redButtonTapped:(UIButton *)sender {
+    UIViewController *redViewController = [[self storyboard] instantiateViewControllerWithIdentifier:@"redScene"];
+    redViewController.view.backgroundColor = [UIColor redColor];
+    [self presentViewController:redViewController
+                       animated:YES
+                     completion:nil];
+}
+- (IBAction)greenButtonTapped:(UIButton *)sender {
+    [self performSegueWithIdentifier:@"showGreenScene"
+                              sender:self];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
