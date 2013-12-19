53e7240dbe00ec82d787063fafa8ee6612f3c696
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-18 20:25:21 -0500

    Set the class for the storyboard to DSTColorViewController.

--- 03Forward/Forward5/Colors/DSTColorViewController.h
+++ 03Forward/Forward5/Colors/DSTColorViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface DSTColorViewController : UIViewController
-
+@property (strong, nonatomic) NSString *displayText;
+@property (strong, nonatomic) UIColor *displayColor;
 @end
--- 03Forward/Forward5/Colors/DSTColorViewController.m
+++ 03Forward/Forward5/Colors/DSTColorViewController.m
@@ -9,41 +9,16 @@
 #import "DSTColorViewController.h"
 
 @interface DSTColorViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *colorLabel;
 @end
 
 @implementation DSTColorViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.view.backgroundColor = self.displayColor;
+    self.colorLabel.text = self.displayText;
+    self.colorLabel.textColor = self.displayColor;
 }
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
