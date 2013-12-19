071475568215fed2d080899157d2d727034ab318
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-18 19:47:42 -0500

    Added project for the fifth stage of chapter three

---
+++ 03Forward/Forward5/Colors/DSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSTAppDelegate.h
+//  Colors
+//
+//  Created by Daniel Steinberg on 12/17/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 03Forward/Forward5/Colors/DSTViewController.m
@@ -0,0 +1,50 @@
+//
+//  DSTViewController.m
+//  Colors
+//
+//  Created by Daniel Steinberg on 12/17/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import "DSTViewController.h"
+
+@interface DSTViewController ()
+
+@end
+
+@implementation DSTViewController
+- (IBAction)redButtonTapped:(UIButton *)sender {
+    UIViewController *redViewController = [[self storyboard] instantiateViewControllerWithIdentifier:@"redScene"];
+    redViewController.view.backgroundColor = [UIColor redColor];
+    [self presentViewController:redViewController
+                       animated:YES
+                     completion:nil];
+}
+- (IBAction)greenButtonTapped:(UIButton *)sender {
+    [self performSegueWithIdentifier:@"showGreenScene"
+                              sender:self];
+}
+- (void)prepareForSegue:(UIStoryboardSegue *)segue
+                 sender:(id)sender {
+    UIViewController *newViewController = segue.destinationViewController;
+    if ([segue.identifier isEqualToString:@"showGreenScene"]) {
+        newViewController.view.backgroundColor = [UIColor greenColor];
+    } else if ([segue.identifier isEqualToString:@"showBlueScene"]) {
+        newViewController.view.backgroundColor = [UIColor blueColor];
+    }
+    
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
