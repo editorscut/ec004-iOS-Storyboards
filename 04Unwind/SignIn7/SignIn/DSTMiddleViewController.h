a17976f673a2953c099044cda0ebc8ff37c3f0f7
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-20 08:51:19 -0500

    Added demo of danger of non-unique names.

---
+++ 04Unwind/SignIn7/SignIn/DSTMiddleViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSTMiddleViewController.h
+//  SignIn
+//
+//  Created by Daniel Steinberg on 12/20/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSTMiddleViewController : UIViewController
+
+@end
---
+++ 04Unwind/SignIn7/SignIn/DSTMiddleViewController.m
@@ -0,0 +1,21 @@
+//
+//  DSTMiddleViewController.m
+//  SignIn
+//
+//  Created by Daniel Steinberg on 12/20/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import "DSTMiddleViewController.h"
+
+@interface DSTMiddleViewController ()
+
+@end
+
+@implementation DSTMiddleViewController
+
+- (IBAction)cancelSignIn:(UIStoryboardSegue *)segue {
+    NSLog(@"cancelSignIn: in DSTMiddleViewController");
+}
+
+@end
