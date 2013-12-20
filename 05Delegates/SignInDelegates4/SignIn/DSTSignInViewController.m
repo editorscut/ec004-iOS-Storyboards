71d7480b6cb11626fb9eba9354b7a1278b1ab2fd
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-20 10:51:21 -0500

    Created final stage of the delegates sequence.

---
+++ 05Delegates/SignInDelegates4/SignIn/DSTSignInViewController.m
@@ -0,0 +1,24 @@
+#import "DSTSignInViewController.h"
+
+@interface DSTSignInViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *signInField;
+@end
+
+@implementation DSTSignInViewController
+- (void)submitSignInName {
+    [self.delegate signInViewController:self
+                           didEnterName:self.signInField.text];
+}
+- (IBAction)saveButtonTapped:(UIButton *)sender {
+    [self submitSignInName];
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self submitSignInName];
+    return YES;
+}
+
+- (IBAction)cancelButtonTapped:(UIButton *)sender {
+    [self.delegate signInViewControllerDidCancel:self];
+}
+
+@end
