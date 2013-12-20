3923866978f256a715ef732efce2cb4d4becd4e4
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-19 20:30:44 -0500

    Added project for stage 5 of SignIn

---
+++ 04Unwind/SignIn5/SignIn/DSTSignInViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface DSTSignInViewController : UIViewController
+@property (strong, nonatomic, readonly) NSString *signInName;
+@end
---
+++ 04Unwind/SignIn5/SignIn/DSTSignInViewController.m
@@ -0,0 +1,21 @@
+#import "DSTSignInViewController.h"
+
+@interface DSTSignInViewController ()
+@property (strong, nonatomic, readwrite) NSString *signInName;
+@property (weak, nonatomic) IBOutlet UITextField *signInField;
+@end
+
+@implementation DSTSignInViewController
+- (IBAction)saveButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)cancelButtonTapped:(UIButton *)sender {
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue
+                 sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"completeSignIn"]) {
+        self.signInName = self.signInField.text;
+    }
+}
+@end
