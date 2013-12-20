8b5a06c43e8bb6216830a58fd003c269f24b1aca
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-20 18:24:53 -0500

    Project is broken - half way through using singleton.

---
+++ 07Model/SignInModel8/SignIn/DSTViewController.m
@@ -0,0 +1,30 @@
+#import "DSTViewController.h"
+#import "DSTSignInViewController.h"
+#import "DSTGuest.h"
+
+@interface DSTViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *greetingLabel;
+@property (strong, nonatomic) DSTGuest *model;
+@end
+
+@implementation DSTViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.model = [[DSTGuest alloc] init];
+}
+
+- (IBAction)cancelSignIn:(UIStoryboardSegue *)segue {
+    NSLog(@"cancelSignIn: in DSTViewController");
+}
+- (IBAction)completeSignIn:(UIStoryboardSegue *)segue {
+    NSLog(@"completeSignIn: in DSTViewController");
+    self.greetingLabel.text = self.model.signInName;
+}
+- (void)prepareForSegue:(UIStoryboardSegue *)segue
+                 sender:(id)sender {
+    DSTSignInViewController *signInVC = segue.destinationViewController;
+    signInVC.model = self.model;
+}
+
+@end
