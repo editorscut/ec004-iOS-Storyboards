a36ebaa373fdceef5d6c5fbbc5a3c8447e274a2e
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-20 14:28:33 -0500

    Preparing project for adding a Model

---
+++ 07Model/SignInModel2/SignIn/DSTSignInViewController.m
@@ -0,0 +1,24 @@
+#import "DSTSignInViewController.h"
+
+@interface DSTSignInViewController ()
+@property (strong, nonatomic, readwrite) NSString *signInName;
+@property (weak, nonatomic) IBOutlet UITextField *signInField;
+@end
+
+@implementation DSTSignInViewController
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue
+                 sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"completeSignIn"]) {
+    }
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.signInField becomeFirstResponder];
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self performSegueWithIdentifier:@"completeSignIn"
+                              sender:textField];
+    return YES;
+}
+@end
---
+++ 07Model/SignInModel2/SignIn/DSTViewController.m
@@ -0,0 +1,17 @@
+#import "DSTViewController.h"
+
+@interface DSTViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *greetingLabel;
+@end
+
+@implementation DSTViewController
+
+- (IBAction)cancelSignIn:(UIStoryboardSegue *)segue {
+    NSLog(@"cancelSignIn: in DSTViewController");
+}
+- (IBAction)completeSignIn:(UIStoryboardSegue *)segue {
+    NSLog(@"completeSignIn: in DSTViewController");
+    
+}
+
+@end
