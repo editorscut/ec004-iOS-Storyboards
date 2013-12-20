6ad45e6681b5d3a28c384d158707710c60f40e65
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-20 10:18:53 -0500

    Created the delegate protocol.

---
+++ 05Delegates/SignInDelegates1/SignIn/DSTSignInViewController.m
@@ -0,0 +1,14 @@
+#import "DSTSignInViewController.h"
+
+@interface DSTSignInViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *signInField;
+@end
+
+@implementation DSTSignInViewController
+- (IBAction)saveButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)cancelButtonTapped:(UIButton *)sender {
+}
+
+@end
