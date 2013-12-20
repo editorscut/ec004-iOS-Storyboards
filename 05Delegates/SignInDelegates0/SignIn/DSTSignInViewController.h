900b6e145c5f681094049997f9297cc99d0ecdbb
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-20 09:48:18 -0500

    Copied over starting point for Delegates chapter

---
+++ 05Delegates/SignInDelegates0/SignIn/DSTSignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSTSignInViewController.h
+//  SignIn
+//
+//  Created by Daniel Steinberg on 12/19/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSTSignInViewController : UIViewController
+
+@end
