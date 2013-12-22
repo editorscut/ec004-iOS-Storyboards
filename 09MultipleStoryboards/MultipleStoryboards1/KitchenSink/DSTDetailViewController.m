d262eca415238dd8d9bb7e91c8f83a14d690e71e
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-22 16:32:56 -0500

    Added preparation step

---
+++ 09MultipleStoryboards/MultipleStoryboards1/KitchenSink/DSTDetailViewController.m
@@ -0,0 +1,16 @@
+#import "DSTDetailViewController.h"
+
+@interface DSTDetailViewController ()
+
+@end
+
+@implementation DSTDetailViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.navigationItem.title = self.titleText;
+}
+
+@end
