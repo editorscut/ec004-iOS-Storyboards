1f5363e71c4c8e66e3f10a7b743b48034c5da57f
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-23 13:30:26 -0500

    Connecting to a second storyboard.

--- 09MultipleStoryboards/MultipleStoryboards3/KitchenSink/DSTDetailViewController.m
+++ 09MultipleStoryboards/MultipleStoryboards3/KitchenSink/DSTDetailViewController.m
@@ -1,14 +1,20 @@
 #import "DSTDetailViewController.h"
+#import "DSTViewController.h"
 
 @interface DSTDetailViewController ()
-
 @end
 
 @implementation DSTDetailViewController
 
+- (IBAction)nextButtonTapped:(UIBarButtonItem *)nextButton {
+    UIStoryboard *signInStoryboard = [UIStoryboard storyboardWithName:@"SignIn"
+                                                               bundle:[NSBundle mainBundle]];
+    DSTViewController *viewController = [signInStoryboard instantiateInitialViewController];
+    [self.navigationController pushViewController:viewController
+                                         animated:YES];
+}
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = self.titleText;
 }
