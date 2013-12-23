e011c03e4ce98c6adc2c603699f7346e97952506
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-23 07:52:17 -0500

    Added copies of KitchenSink for next two stages

---
+++ 09MultipleStoryboards/MultipleStoryboards3/KitchenSink/DSTTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DSTTableViewController.h
+//  KitchenSink
+//
+//  Created by Daniel Steinberg on 12/21/13.
+//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSTTableViewController : UITableViewController
+
+@end
