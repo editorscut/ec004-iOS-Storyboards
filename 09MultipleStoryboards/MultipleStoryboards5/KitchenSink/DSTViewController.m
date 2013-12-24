94af6e6c016c3d9a4a76a64e8b1637a5dbf96881
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-24 14:26:13 -0500

    Finished sequence to add a scene from a second storyboard.

--- 09MultipleStoryboards/MultipleStoryboards5/KitchenSink/DSTViewController.m
+++ 09MultipleStoryboards/MultipleStoryboards5/KitchenSink/DSTViewController.m
@@ -6,9 +6,23 @@
 @end
 
 @implementation DSTViewController
+- (IBAction)signInTapped:(UIButton *)sender {
+    UIStoryboard *signInStoryboard = [UIStoryboard storyboardWithName:@"SignIn"
+                                                               bundle:[NSBundle mainBundle]];
+    DSTSignInViewController *signInVC = [signInStoryboard instantiateViewControllerWithIdentifier:@"SignInScene"];
+    [self setBlocksFor:signInVC];
+    [self presentViewController:signInVC
+                       animated:YES
+                     completion:nil];
+}
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue
                  sender:(id)sender {
     DSTSignInViewController *signInVC = segue.destinationViewController;
+    [self setBlocksFor:signInVC];
+}
+
+- (void)setBlocksFor:(DSTSignInViewController *)signInVC {
     signInVC.cancelTextEntry = ^{
         [self dismissViewControllerAnimated:YES
                                  completion:nil];
