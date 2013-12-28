8bc7124c8bd7ed8e2ef46fb26637a9b4a6fa312e
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-28 08:22:53 -0500

    Finished adding code to Table view example

--- 10TableViews/OneThing2/OneThing/DSTMasterViewController.m
+++ 10TableViews/OneThing2/OneThing/DSTMasterViewController.m
@@ -1,6 +1,7 @@
 #import "DSTMasterViewController.h"
 #import "DSTDetailViewController.h"
 #import "DSTEvent.h"
+#import "DSTInputViewController.h"
 
 @interface DSTMasterViewController ()
 @end
@@ -16,14 +17,9 @@
     [super awakeFromNib];
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DSTDetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
@@ -33,26 +29,22 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender
-{
+- (IBAction)insertNewEvent:(UIStoryboardSegue *)segue {
+    DSTInputViewController *inputVC = segue.sourceViewController;
     NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
     NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
-    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-    
-    // If appropriate, configure the new managed object.
-    // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
-    
-    // Save the context.
+    DSTEvent *event = [NSEntityDescription insertNewObjectForEntityForName:[entity name]
+                                                    inManagedObjectContext:context];
+    event.title = inputVC.title;
+    event.eventDescription = inputVC.eventDescription;
+    event.timeStamp = [NSDate date];
     NSError *error = nil;
     if (![context save:&error]) {
-         // Replace this implementation with code to handle the error appropriately.
-         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
 }
-
+- (IBAction)cancelInsertNewEvent:(UIStoryboardSegue *)segue {}
 #pragma mark - Table View
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
