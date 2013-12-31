ea7bae8c154e60521c0c835be96b6c75c108fa3d
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-31 18:49:12 -0500

    Removed comments from chapter 5

--- 05Delegates/SignInDelegates4/SignIn/DSTAppDelegate.m
+++ 05Delegates/SignInDelegates4/SignIn/DSTAppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  DSTAppDelegate.m
-//  SignIn
-//
-//  Created by Daniel Steinberg on 12/19/13.
-//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
-//
-
 #import "DSTAppDelegate.h"
 
 @implementation DSTAppDelegate
