3906510e9886de0d883be57bbf89ef52f8f33994
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-31 18:29:26 -0500

    Stripped comments from chapter 2

--- 02Transitions/Colors3/Colors/DSTAppDelegate.h
+++ 02Transitions/Colors3/Colors/DSTAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  DSTAppDelegate.h
-//  Colors
-//
-//  Created by Daniel Steinberg on 12/17/13.
-//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface DSTAppDelegate : UIResponder <UIApplicationDelegate>
--- 02Transitions/Colors3/Colors/DSTViewController.h
+++ 02Transitions/Colors3/Colors/DSTViewController.h
@@ -1,11 +1,3 @@
-//
-//  DSTViewController.h
-//  Colors
-//
-//  Created by Daniel Steinberg on 12/17/13.
-//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface DSTViewController : UIViewController
