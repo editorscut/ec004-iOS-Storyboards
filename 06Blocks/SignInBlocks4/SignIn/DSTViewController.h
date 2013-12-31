e2b88ccb478a0eed21be18321dd1b54f5151c22c
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-31 18:54:19 -0500

    Stripped comments for chapter 6

--- 06Blocks/SignInBlocks4/SignIn/DSTViewController.h
+++ 06Blocks/SignInBlocks4/SignIn/DSTViewController.h
@@ -1,11 +1,3 @@
-//
-//  DSTViewController.h
-//  SignIn
-//
-//  Created by Daniel Steinberg on 12/19/13.
-//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface DSTViewController : UIViewController
