2b820e2db70d14f302b139e81e238302758afe21
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-01 08:44:59 -0500

    removed duplicate project

--- 09MultipleStoryboards/SignInBlocks4/SignIn/DSTSignInViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-typedef void(^DSTDismissView)(void);
-typedef void(^DSTTextInputHandler)(NSString *text);
-
-@interface DSTSignInViewController : UIViewController
-@property (copy, nonatomic) DSTDismissView cancelTextEntry;
-@property (copy, nonatomic) DSTTextInputHandler completeTextEntry;
-@end
