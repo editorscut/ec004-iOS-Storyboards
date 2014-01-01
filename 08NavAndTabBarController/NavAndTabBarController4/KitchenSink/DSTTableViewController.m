69298e34200104939c3d7458ae8ae6d698cadaec
Daniel Steinberg <daniel@dimsumthinking.com>
2013-12-31 19:11:55 -0500

    Stripped comments from chapter 8

--- 08NavAndTabBarController/NavAndTabBarController4/KitchenSink/DSTTableViewController.m
+++ 08NavAndTabBarController/NavAndTabBarController4/KitchenSink/DSTTableViewController.m
@@ -1,11 +1,3 @@
-//
-//  DSTTableViewController.m
-//  KitchenSink
-//
-//  Created by Daniel Steinberg on 12/21/13.
-//  Copyright (c) 2013 Dim Sum Thinking. All rights reserved.
-//
-
 #import "DSTTableViewController.h"
 
 @interface DSTTableViewController ()
